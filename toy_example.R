974ef8d90d58ec786ad1edfc9de5c9a716754342
jasonzhao0307 <jasonzhao0307@gmail.com>
2018-06-24 13:14:12 -0400

    start working on model selection

---
+++ toy_example.R
@@ -0,0 +1,44 @@
+# load all functions
+source("model_selection.R")
+
+
+
+########   Example   ########
+
+# load example dataset
+# we have 4 objects here:
+# - df.training, with rows being samples, columns being variables.
+# - df.testing, with rows being samples, columns being variables.
+# - y.train, a binary vector
+# - y.test, a binary vector
+data.example <- readRDS("model_selection_example_data.RDS")
+
+
+# prepare the data to make the format useable by the pipeline
+# [Important!] User needs to specify which class in y is the positive class
+data.prepared <- prepareData(df.training = data.example$df.training,
+                             df.testing = data.example$df.testing,
+                             y.train = data.example$y.train,
+                             y.test = data.example$y.test,
+                             positive = "cancer")
+
+## Data normalization
+data.norm <- normData(training = data.prepared$training,
+                      testing = data.prepared$testing)
+
+
+### Model selection
+
+model.selection.output <- modelSelection(df.training = data.norm$trainTransformed,
+                      model.names = c("gbm", "svmLinear"),
+                      num.cv.fold = 5,
+                      num.cv.repeat = 5,
+                      num.param.tune = 6,
+                      seed.use = 800,
+                      show.some.models = TRUE)
+
+## Prediction on testing set and evaluation
+## [Note]: user needs to provide the prevalence for the positive case
+test.output <- predictAndEvaluation(model.best = model.selection.output$model.best,
+                                    test.data = data.norm$testTransformed,
+                                    prevalence = 0.1)
